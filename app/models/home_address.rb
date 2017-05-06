37584d12e2cfbdee710ea86b2c54b87fb162d317
Sato Junichi <villanova911@gmail.com>
2017-05-06 23:18:56 +0900

    introduce capybara

--- app/models/home_address.rb
+++ app/models/home_address.rb
@@ -1,1 +1,4 @@
-class HomeAddress < Address; end
+class HomeAddress < Address
+  validates :postal_code, :prefecture, :city, :address1, presence: true
+  validates :company_name, :division_name, absence: true
+end
